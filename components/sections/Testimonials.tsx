7525ff7a7049846fcebe85d4b942e55fc28f6bec
Aminur Muda <aminur.muda@wartek.belajar.id>
2026-03-10 09:10:23 +0700

    translate testimonials to english

--- components/sections/Testimonials.tsx
+++ components/sections/Testimonials.tsx
@@ -1,41 +1,42 @@
 const testimonials = [
-    {
-        id: 1,
-        name: 'Student',
-        testimonial: 'Aku punya masalah banget di speaking. Tapi karena ikut kelas Speaking bareng Ms. Anna, meski bahasaku belum sempurna, alhamdulillah jadi bisa lebih pede ngomong bahasa Inggris! Ms. Anna sabar banget dengering bahasa Inggrisku yang masih berantakan :") THank you Miss!',
-        role: 'Mahasiswa'
-    },
-    {
-        id: 2,
-        name: 'Student',
-        testimonial: 'Kesan saya dalam mengikuti kelas daring sangat menyenangkan sekali karena mentor selain berpengalaman juga sangat amat mengajari dengan teliti',
-        role: 'Mahasiswa Magister'
-    },
-    {
-        id: 3,
-        name: 'Student',
-        testimonial: 'Saya sangat senang mengikuti kelas daring bahasa Inggris di MEC. Materinya mudah dipahami, dan pengajarnya sangat interaktif dan membantu. Saya merasa kemampuan bahasa inggris saya meningkat pesat setelah mengikuti kelas ini.',
-        role: 'Pekerja Swasta'
-    },
-    {
-        id: 4,
-        name: 'Student',
-        testimonial: 'After I joined the class I thing my english is getting better. Also learning english with Miss Anna was fun and enjoy. Thank you Miss Anna.',
-        role: 'Guru'
-    },
-    {
-        id: 5,
-        name: 'Student',
-        testimonial: 'Pembelajaran yang jelas dan mudah difahami',
-        role: 'Dosen'
-    },
-    {
-        id: 6,
-        name: 'Student',
-        testimonial: 'seru menyenangkan fleksibel',
-        role: 'Pekerja BUMN'
-    },
+  {
+    id: 1,
+    name: 'Student',
+    testimonial: 'I used to struggle a lot with speaking. But after joining the Speaking class with Ms. Anna, even though my English is still not perfect, I feel much more confident speaking now! Ms. Anna was very patient listening to my messy English :") Thank you, Miss!',
+    role: 'University Student'
+  },
+  {
+    id: 2,
+    name: 'Student',
+    testimonial: 'My experience attending the online class was very enjoyable. The mentor was not only experienced but also taught very carefully and clearly.',
+    role: 'Master’s Student'
+  },
+  {
+    id: 3,
+    name: 'Student',
+    testimonial: 'I really enjoyed joining the online English class at MEC. The materials were easy to understand, and the teacher was very interactive and helpful. I feel that my English skills improved significantly after attending this class.',
+    role: 'Private Sector Employee'
+  },
+  {
+    id: 4,
+    name: 'Student',
+    testimonial: 'After I joined the class I think my English is getting better. Also learning English with Ms. Anna was fun and enjoyable. Thank you Miss Anna.',
+    role: 'Teacher'
+  },
+  {
+    id: 5,
+    name: 'Student',
+    testimonial: 'The lessons were clear and easy to understand.',
+    role: 'Lecturer'
+  },
+  {
+    id: 6,
+    name: 'Student',
+    testimonial: 'Fun, enjoyable, and flexible learning experience.',
+    role: 'State-Owned Enterprise Employee'
+  },
 ]
+
 const Testimonials = () => {
   return (
     <section id="testimonials" className="py-24">

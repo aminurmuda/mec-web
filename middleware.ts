f22e980750f834245fe1041f875b6fb6b24b924e
Aminur Muda <aminur.muda@wartek.belajar.id>
2026-04-09 08:39:50 +0700

    fix: image not displayed because middleware

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,14 @@ const defaultLocale = 'en';
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
+  if (
+    pathname.startsWith('/api') ||
+    pathname.startsWith('/_next') ||
+    pathname.startsWith('/images')
+  ) {
+    return;
+  }
+
   const pathnameHasLocale = locales.some((locale) => pathname.startsWith(`/${locale}`));
 
   if (!pathnameHasLocale) {
@@ -15,5 +23,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next|favicon.ico).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };

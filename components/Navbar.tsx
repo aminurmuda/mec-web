ad95141aa2255748546e344715a7b38446b39f33
Aminur Muda <aminur.muda@wartek.belajar.id>
2026-04-14 13:01:14 +0700

    feat: add profile and logout in sidebar mobile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,6 +6,8 @@ import LanguageSwitcher from './LanguageSwitcher';
 import { useLocale } from '@/context/LocaleContext';
 import NavbarProfile from './NavbarProfile';
 import Link from 'next/link';
+import { signOut, useSession } from 'next-auth/react';
+import Image from 'next/image';
 
 const leagueSpartan = League_Spartan({
   subsets: ['latin'],
@@ -13,8 +15,15 @@ const leagueSpartan = League_Spartan({
 });
 
 const Navbar = () => {
-  const { locale } = useLocale();
+  const { data: session } = useSession();
+  const { locale, redirect } = useLocale();
   const [open, setOpen] = useState(false);
+
+  const handleLogout = () => {
+    signOut({ callbackUrl: '/' });
+  };
+
+  const isAuthenticated = session?.user?.email !== undefined;
 
   const menuEn = [
     { name: 'Courses', id: 'courses' },
@@ -74,8 +83,16 @@ const Navbar = () => {
                 {item.name}
               </button>
             ))}
+            {isAuthenticated && (
+              <button
+                onClick={() => redirect('/courses')}
+                className="hover:text-white transition text-brand-primary font-semibold text-lg"
+              >
+                Edit Course
+              </button>
+            )}
           </div>
-          <div className="hidden md:flex">
+          <div className="hidden md:flex items-center justify-center">
             {process.env.NEXT_PUBLIC_SHOW_LANGUAGE_SWITCHER === 'true' && <LanguageSwitcher />}
             <NavbarProfile />
           </div>
@@ -111,6 +128,37 @@ const Navbar = () => {
               </button>
             ))}
           </div>
+          {!!session && (
+            <div className="border border-t border-gray-200 px-5 py-4">
+              <div className="flex items-center justify-between gap-4">
+                <div className="flex items-center gap-4">
+                  {session.user?.image ? (
+                    <Image
+                      src={session.user.image}
+                      alt={session.user.name || 'User'}
+                      width={36}
+                      height={36}
+                      className="rounded-full"
+                    />
+                  ) : (
+                    <div className="w-8 h-8 flex items-center justify-center bg-gray-300 rounded-full text-sm font-medium">
+                      {session.user?.name?.charAt(0)}
+                    </div>
+                  )}
+                  <div>
+                    <p className="font-semibold text-brand-primary">{session?.user?.name}</p>
+                    <p className="text-brand-primary mt-1 truncate">{session?.user?.email}</p>
+                  </div>
+                </div>
+                <span
+                  onClick={handleLogout}
+                  className="font-semibold text-left py-2.5 text-red-500 hover:bg-red-50 transition"
+                >
+                  Log out
+                </span>
+              </div>
+            </div>
+          )}
         </div>
       )}
     </nav>
